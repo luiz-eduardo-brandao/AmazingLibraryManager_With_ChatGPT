f159efdc19ff28376b912f04d53f1fb9cc156408
Luiz Eduardo <133886047+luiz-eduardo-brandao@users.noreply.github.com>
2024-01-23 19:47:01 -0300

    creating new api gateway project

--- AmazingLibraryManager.BooksCatalog/AmazingLibraryManager.BooksCatalog.Infrastructure/obj/Debug/net7.0/AmazingLibraryManager.BooksCatalog.Infrastructure.AssemblyInfo.cs
+++ AmazingLibraryManager.BooksCatalog/AmazingLibraryManager.BooksCatalog.Infrastructure/obj/Debug/net7.0/AmazingLibraryManager.BooksCatalog.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AmazingLibraryManager.BooksCatalog.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52f6cd8824173a95685fc3a42e9d04a5bce63f25")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("AmazingLibraryManager.BooksCatalog.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AmazingLibraryManager.BooksCatalog.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
